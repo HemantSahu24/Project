5135747dce88faafc59dd949e525231790cb7ddf
Hemant Sahu <hemantsahu211020@gmail.com>
2021-11-05 10:56:10 +0530

    making it better

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,49 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+import otpGenerator from 'otp-generator'
 import UserModal from "../models/user.js";
 import LoginModal from "../models/loginUsers.js"
+import nodemailer from 'nodemailer';
 
 const secret = `${process.env.secret_key}`;
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'quickshare56@gmail.com',
+    pass: 'Quick30101997'
+  }
+});
 
+export const requestOtp=async(req,res)=>{
+  const op=otpGenerator.generate(4, { alphabets:false,digits:true,upperCase:false,specialChars:false });
+  // console.log(op);
+  try{
+    const oldUser = await UserModal.findOne({email: req.body.email });
+
+    if (oldUser) return res.status(400).json({ message: "User already exists" });
+  var mailOptions = {
+    from: 'QuickShare <quickshare56@gmail.com>',
+    to: `${req.body.email}`,
+    subject: `QuickShare Email Verification`,
+    html: `<p>Hi,</p> <p> Your OTP for verifying email on QuickShare is ${op}.</p>`
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.status(401).send("This email id doesn't exist,try with another")
+    } else {
+      res.status(200).send(op);
+      console.log('Email sent: ' + info.response);
+    }
+  });
+ 
+}
+catch(error){
+    res.status(401).send('Something went wrong');
+}
+
+
+}
 export const signin = async (req, res) => {
   const { email, password } = req.body;
 
@@ -40,8 +78,24 @@ export const signup = async (req, res) => {
     const result = await UserModal.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
     const token = jwt.sign( { email: result.email, id: result._id }, secret, { expiresIn: "1h" } );
-
+    var mailOptions = {
+      from: 'QuickShare <quickshare56@gmail.com>',
+      to: `${email}`,
+      subject: `Welcome To QuickShare`,
+      html: `<p>Hi ${firstName},</p> <p> Thank you for registering on QuickShare.Make memories and start sharing on QuickShare.</p>`
+    };
+    
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+         console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+   
     res.status(201).json({ result, token });
+   
+
   } catch (error) {
     res.status(500).json({ message: "Something went wrong" });
     
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup,loginUser } from "../controllers/user.js";
+import { signin, signup,loginUser, requestOtp } from "../controllers/user.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
 router.post("/loginUsers",loginUser);
+router.post("/requestotp",requestOtp);
 
 export default router;
