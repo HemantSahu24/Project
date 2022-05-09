d5274abb3739eac8bdd68beaf7a6bd91c9fd323d
Hemant Sahu <hemantsahu211020@gmail.com>
2022-05-09 17:20:37 +0530

    better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // app.use(cors());
 const corsOptions ={
-    origin:'https://hemant-sahu.netlify.app', 
+    origin:'*', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
