a264a405428cf2101221d88bc17e004a49cbf02b
Deepakkumarsen <deepakkumarsen2002@gmail.com>
2022-12-02 09:58:28 +0530

    better

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 // app.use(cors());
 const corsOptions ={
-    origin:'localhost:3000', 
+    origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
