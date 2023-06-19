7df51ee6d7ee1a7e0a098624638e0710908e36a4
Hemant <sahuhemant770@gmail.com>
2023-06-19 14:43:44 +0530

    update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ dotenv.config();
 const PORT = process.env.PORT || 7000
 
 
-mongoose.connect("mongodb+srv://My-proj:vLdrF9ZcEHqeSaG@myproj.bqul5.mongodb.net/TempProj?retryWrites=true&w=majority",
+mongoose.connect(process.env.con_url,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
